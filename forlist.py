8bcd59d0ce99d1e87888d15abd6adc9c4511cf97
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-08-09 15:17:22 +0530

    secondcommit

---
+++ forlist.py
@@ -0,0 +1,14 @@
+for_list=[1,2,3,4,5,6,7,8,9,10]
+for x in for_list:
+    print(x)
+    print(x*x)
+    print(x*x*x)
+    print(x*x*x*x)
+for_list=[["Anu",21,"place","Luminar"],["Achu",21,"place","Luminar"],["praju",22,"place","Luminar"]]
+for i in for_list:
+    print(i[0],"is a good student")
+    print(i[1],"is the age of student")
+    print(i[2],"is the place of student")
+    print(i[3],"is the institute of student")
+    print("\n")
+    print("Next Student")
