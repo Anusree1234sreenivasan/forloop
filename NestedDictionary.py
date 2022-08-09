1242be8445f24bbad7543430680b4f69cfbb15a3
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-08-09 15:25:00 +0530

    fourth

---
+++ NestedDictionary.py
@@ -0,0 +1,24 @@
+nested_dictionary={
+    "0ne":
+    {
+    "name":"Anu",
+    "age":21,
+    "city":"New York"
+ },
+    "two":
+{
+     "name":"Sree",
+     "age":31,
+     "city":"paris"
+},
+    "three":
+{
+     "name":"shamma",
+     "age":22,
+     "city":"London"
+}
+}
+print((nested_dictionary))
+
+print(nested_dictionary)
+print(nested_dictionary["three"]["name"])
